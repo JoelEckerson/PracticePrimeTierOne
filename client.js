57100140b9940c56c060cb5662ac3696de9364f5
Joel Eckerson <>
2021-02-26 18:54:45 -0600

    Done with First Video

--- client.js
+++ client.js
@@ -1,6 +1,21 @@
 $( document ).ready( readyNow );
 
 const budget = 25000;
+let purchases = [];
+
+function addPurchase(){
+  // get user imput create new Object
+  let newPurchase = {
+    name: $( '#purchaseNameIn' ).val(),
+    price: $ ( '#purchasePriceIn' ).val()
+  }
+  //push new purchase into the array
+  purchases.push( newPurchase );
+  //empty inputs
+  $( '#purchaseNameIn' ).val( '' );
+  $ ( '#purchasePriceIn' ).val( '' );
+  //calculate remaingBudget
+} // end addPurchase
 
 function readyNow(){
   // display budgetOut
@@ -8,4 +23,6 @@ function readyNow(){
   let el = $( '#budgetOut' );
   el.empty();
   el.append( budget );
+  // handle click event
+  $( '#addPurchaseButton' ).on( 'click', addPurchase );
 }//end readyNow
